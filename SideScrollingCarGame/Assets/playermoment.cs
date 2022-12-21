41e567a6521e1bf505b41319f6c93c478c9c7e01
alekaixd <aleksi.pirttinen@gmail.com>
2022-12-21 18:47:46 +0200

    sexed your code

--- SideScrollingCarGame/Assets/playermoment.cs
+++ SideScrollingCarGame/Assets/playermoment.cs
@@ -5,30 +5,18 @@ using UnityEngine;
 public class playermoment : MonoBehaviour
 {
     public float moveSpeed;
-
     public Rigidbody2D rb;
-
     private Vector2 moveDirection;
 
     // Update is called once per frame
     void Update()
-    {
-        ProcessInputs();
-    }
-    void FixedUpdate()
-    {
-        Move();
-    }
-
-    void ProcessInputs()
     {
         float MoveX = Input.GetAxisRaw("Horizontal");
         float MoveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(MoveX, MoveY);
     }
-
-    void Move()
+    void FixedUpdate()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
