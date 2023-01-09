65c26334780e4ed700dc6923e2104df30ed35247
alekaixd <aleksi.pirttinen@gmail.com>
2023-01-10 00:08:30 +0200

    d

--- SideScrollingCarGame/Assets/scripts/GameManager.cs
+++ SideScrollingCarGame/Assets/scripts/GameManager.cs
@@ -6,21 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public int gameScore = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void AddScore(int score) // adds score
     {
         gameScore += score;
         Debug.Log("added score: " + gameScore);
         GameObject.Find("ScoreText").GetComponent<TMPcontroller>().UpdateScore(gameScore);
+    }
+
+    public void GameOver()
+    {
+        //change scene
     }
 }
